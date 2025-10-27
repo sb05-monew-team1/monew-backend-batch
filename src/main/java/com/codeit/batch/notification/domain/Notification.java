6be0b003fc5bce22c290f6af6f0f26e007970cc0
rlawnsry <color9117@naver.com>
2025-10-27 17:05:11 +0900

    feat: entity 추가

---
+++ src/main/java/com/codeit/batch/notification/domain/Notification.java
@@ -0,0 +1,46 @@
+package com.codeit.batch.notification.domain;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.codeit.batch.common.base.BaseUpdatableDomain;
+import com.codeit.batch.user.domain.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Table(name = "notifications")
+@SuperBuilder
+@Setter @Getter
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+public class Notification extends BaseUpdatableDomain {
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@Column(nullable = false)
+	private boolean confirmed;
+
+	@Column(nullable = false, length = 500)
+	private String content;
+
+	@Column(name = "resource_type", nullable = false, length = 20)
+	private String resourceType;
+
+	@Column(name = "resource_id", nullable = false)
+	private UUID resourceId;
+}
---
+++ src/main/java/com/codeit/batch/user/domain/User.java
@@ -0,0 +1,60 @@
+package com.codeit.batch.user.domain;
+
+import java.time.Instant;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.codeit.batch.common.base.BaseUpdatableDomain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@Entity
+@Table(name = "users")
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //접근 레벨 PROTECTED
+@EntityListeners(AuditingEntityListener.class)
+public class User extends BaseUpdatableDomain {
+
+  @Column(nullable = false, unique = true) // 'nullable = false': NOT NULL, 'unique = true': UNIQUE 제약조건
+  private String email;
+
+  @Column(nullable = false)
+  private String nickname;
+
+  @Column(nullable = false)
+  private String password; //암호화 되어야 함.
+
+  //논리 삭제시간
+  private Instant deletedAt;
+
+  //회원가입을 위한 User 객체 생성
+  public static User register(String email, String nickname, String password) {
+    return User.builder()
+        .email(email)
+        .nickname(nickname)
+        .password(password)
+        // id, createdAt, updatedAt은 부모 클래스와 Auditing이 자동 처리
+        .build();
+  }
+
+  //비지니스 로직 메서드
+  //닉네임 수정
+  public void updateNickname(String nickname) {
+
+    this.nickname = nickname;
+  }
+
+	//논리 삭제 처리
+	//논리삭제시 1일 뒤 물리 삭제
+	public void softDelete() {
+		this.deletedAt = Instant.now();
+	}
+}
